906fe90fcb87c838f174d180519ee4263d33e512
WIZI18 <154735139+WIZI18@users.noreply.github.com>
2024-05-16 16:02:18 +0500

    light bulb

---
+++ app.js
@@ -0,0 +1,4 @@
+var image = document.getElementById("image")
+image.addEventListener("click",function(){
+    image.src = "https://media.istockphoto.com/id/1149246206/vector/light-bulb-with-rays-lighting-electric-lamp-creative-idea-solution-thinking-concept.jpg?s=612x612&w=0&k=20&c=MKFLk7zJSL1LcS1E91ujw8hnoMdAj5scM_QOEhiIkgI=";
+});
